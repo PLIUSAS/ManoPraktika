f729e8f957f324f24a5df33f6839eaeb332fc127
PLIUSAS <Laurynas1297@gmail.com>
2023-12-08 22:19:40 +0200

    ..

--- Pirma Pamoka/Backend/server.js
+++ Pirma Pamoka/Backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import routes from "./routes/routes";
+import routers from "./routers/routers.js";
 
 dotenv.config();
 
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(routers);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
