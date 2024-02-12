7f288f061a1b441701c82213562b968844476e68
PLIUSAS <Laurynas1297@gmail.com>
2024-02-12 17:38:00 +0200

    Pradžia

---
+++ Onex.lt Projektas/Frontend/onex/src/components/Medikai/Medikai.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Medikai() {
+  return <div></div>;
+}
---
+++ Onex.lt Projektas/Frontend/onex/src/components/Pagrindinis/Pagrindinis.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Pagrindinis() {
+  return <div></div>;
+}
